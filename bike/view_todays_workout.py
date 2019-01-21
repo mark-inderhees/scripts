8d20c8a5761cad69a303bebcc1bc7972790354ab
Mark Inderhees <mark.inderhees@gmail.com>
2019-01-20 16:34:46 -0800

    save config for workout dir and start date to disk

--- bike/view_todays_workout.py
+++ bike/view_todays_workout.py
@@ -9,6 +9,7 @@ from PyQt5.QtWidgets import (
     QWidget,
     QVBoxLayout,
     QFileDialog,
+    QLabel,
     QCalendarWidget,
     QPushButton)
 
@@ -18,13 +19,38 @@ class App(QWidget):
     def __init__(self):
         super().__init__()
         self.config = None
-        self.init_ui()
+        self.config_file = None
+        self.directory_name = None
+        self.start_date = None
+        self.calendar = QCalendarWidget()
+        self.vbox = QVBoxLayout()
+        self.setLayout(self.vbox)
+        self.setWindowTitle("Today's Workout")
+        if self.load_config():
+            if self.config is not None:
+                self.init_ui()
+        else:
+            label_error = QLabel(
+                'Failed to load configuration information',
+                self)
+            self.vbox.addWidget(label_error)
+            self.show()
 
     def init_ui(self):
         """Run UI Code"""
-        self.setWindowTitle("Today's Workout")
-        self.setFixedSize(640, 480)
-        self.load_config()
+        self.hide()
+        for i in reversed(range(self.vbox.count())):
+            self.vbox.itemAt(i).widget().setParent(None)
+        self.directory_name = self.config['directory_name']
+        self.start_date = self.config['start_date']
+        label_directory = QLabel(
+            'Directory {}'.format(self.directory_name),
+            self)
+        label_start_date = QLabel(
+            'Start date {}'.format(self.start_date),
+            self)
+        self.vbox.addWidget(label_directory)
+        self.vbox.addWidget(label_start_date)
         self.show()
 
     def open_directory_dialog(self):
@@ -32,44 +58,55 @@ class App(QWidget):
         options = QFileDialog.Options()
         directory_name = QFileDialog.getExistingDirectory(
             self,
-            "Workout Directory",
-            "",
-            options=options)
+            "Select Workout Directory",
+            "", # Start directory, empty for current directory
+            options)
         if directory_name:
             return directory_name
         return None
 
     def build_config(self):
         """Prompt user for config settings and save to disk"""
-        directory_name = self.open_directory_dialog()
-        if directory_name is None:
-            return
+        self.directory_name = self.open_directory_dialog()
+        if self.directory_name is None:
+            return False
 
-        vbox = QVBoxLayout()
-        calendar = QCalendarWidget()
-        calendar.setGridVisible(True)
+        self.hide()
+        self.calendar.setGridVisible(True)
         button = QPushButton("Set Start Date")
-        vbox.addWidget(calendar)
-        vbox.addWidget(button)
-        self.setLayout(vbox)
-        date = calendar.selectedDate()
-        print(date)
+        button.clicked.connect(self.set_date_click)
+        self.vbox.addWidget(self.calendar)
+        self.vbox.addWidget(button)
+        self.show()
 
-        self.config = None
+        return True
+
+    def set_date_click(self):
+        """Calendar set date button click event handler"""
+        start_date = self.calendar.selectedDate()
+        self.config = {}
+        self.config['start_date'] = start_date
+        self.config['directory_name'] = self.directory_name
+        pickle.dump(self.config, open(self.config_file, 'wb'))
+        self.init_ui()
 
     def load_config(self):
         """Read config file off disk or create a new one"""
-        config_file = os.path.abspath(os.path.join(
+        config_directory = os.path.abspath(os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
             '..',
             '..',
-            "etc",
-            "viewTodaysWorkout.config"))
+            "etc"))
+        if not os.path.exists(config_directory):
+            os.mkdir(config_directory)
+        self.config_file = os.path.join(
+            config_directory,
+            "view_todays_workout.b")
 
-        if os.path.exists(config_file):
-            self.config = pickle.load(open(config_file))
-        else:
-            self.build_config()
+        if os.path.exists(self.config_file):
+            self.config = pickle.load(open(self.config_file, 'rb'))
+            return True
+        return self.build_config()
 
 if __name__ == '__main__':
     APP = QApplication(sys.argv)
